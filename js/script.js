1df9be049956f6ed87365f8da26cf4a475f68722
MargaritaOt <50380716+MargaritaOt@users.noreply.github.com>
2019-07-10 23:11:11 +0300

    зд

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
 var search = document.querySelector(".search-link");
-
 var popup = document.querySelector(".search-form");
-
 var close = popup.querySelector(".modal-close");
-
-search.addEventListener("click", function (evt){
-evt.preventDefault();
-popup.classList.add("modal-show");
+search.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
 });
-
-close.addEventListener("click", function (evt){
-evt.preventDefault();
-popup.classList.remove("modal-show");
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
 });
-
-
 var button = document.querySelector("#js-button");
-
-var modal = document.querySelector(".form-close");
-
-var modalClose = popup.querySelector(".form-close");
-
-
-button.addEventListener("click", function (evt){
-evt.preventDefault();
-modal.classList.add("modal-show");
+var modal = document.querySelector(".modal-direction");
+var modalClose = modal.querySelector(".form-close");
+button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.add("modal-show");
 });
-
-close.addEventListener("click", function (evt){
-evt.preventDefault();
-modal.classList.remove("modal-show");
+modalClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal-show");
 });
